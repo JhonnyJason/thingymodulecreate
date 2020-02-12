21a999b7f3e6b656b45003d2cabc607097090756
JhonnyJason <lenny09918050@gmail.com>
2020-02-12 17:07:18 +0000

    update before test

--- testing/test.sh
+++ testing/test.sh
@@ -5,7 +5,7 @@ cd source
 # node ../../output/index.js directory,ultracoolesmodule
 # node ../../output/index.js directory,ultracoolesmodule
 # node ../../output/index.js directory,asdmodule
-node ../../output/index.js directory,ultramodule
+# node ../../output/index.js directory,ultramodule
 # node ../../output/index.js directory,ooomodule
 # node ../../output/index.js directory,dirooomodule
 # node ../../output/index.js directory,testoomodule
@@ -13,7 +13,7 @@ node ../../output/index.js directory,ultramodule
 
 ## construct submodule
 # node ../../output/index.js submodule,ultracoolesmodule,create,void
-# node ../../output/index.js submodule,asdmodule,merge,lenny-unlicense
+node ../../output/index.js submodule,asdmodule,merge,lenny-unlicense
 # node ../../output/index.js submodule,ultramodule,use,lenny-unlicense
 
 ## construct directory
